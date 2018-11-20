2880f9cb9cc5606815d873b81e1e083929ca2f15
Alina <alna.kireeva88@gmail.com>
2018-11-20 15:34:12 +1300

    created hbs files in Views for main pages

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index', { hi: 'Hello World!' })
+  res.render('home', {hi: 'LinkedArt App'})
 })
 
+router.get('/theatres', (req, res) => {
+  res.render('theatres', {page: 'THEATRES'})
+})
+
+router.get('/musicals', (req, res) => {
+  res.render('musicals', {page: 'MUSICALS'})
+})
+
+router.get('/dances', (req, res) => {
+  res.render('dances', {page: 'DANCE CLASSES'})
+})
+
+router.get('/workshops', (req, res) => {
+  res.render('workshops', {page: 'CLASSES AND WORKSHOPS'})
+})
 module.exports = router
--- server.js
+++ server.js
@@ -5,10 +5,24 @@ const path = require('path')
 const routes = require('./routes')
 
 const server = express()
+// configuration
 server.engine('hbs', hbs())
 server.set('view engine', 'hbs')
 server.set('views', path.join(__dirname, 'views'))
-server.use(express.urlencoded({ extended: true }))
+server.use(express.urlencoded({extended: true}))
 server.use('/', routes)
 
+// routes
+server.get('/', (req, res) => {
+  console.log('/route hit')
+  const data = {
+    homePageList: [
+      {name: 'Theatre', color: '', style: ''},
+      {name: 'Musicals', color: '', style: ''},
+      {name: 'Dance Auditions', color: '', style: ''},
+      {name: 'Classes', color: '', style: ''}
+    ]
+  }
+  res.render('home')
+})
 module.exports = server
